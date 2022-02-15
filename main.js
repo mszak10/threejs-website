8c1edec86928dfb2e2a1842da96314700fb95e03
mszak10 <mszak10@gmail.com>
2022-02-15 17:56:13 +0100

    added raycaster

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 
 // Setup
+const raycaster = new THREE.Raycaster();
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -23,10 +25,13 @@ renderer.render(scene, camera);
 // Setting up a Plane
 const geometry = new THREE.PlaneGeometry( 400, 400, 50, 50 );
 const material = new THREE.MeshPhongMaterial({ 
-  color: 0xCF1400,
-side: THREE.DoubleSide,
-flatShading: THREE.FlatShading });
+  //color: 0xCF1400,
+  side: THREE.DoubleSide,
+  flatShading: THREE.FlatShading,
+  vertexColors: true});
 const plane = new THREE.Mesh(geometry, material);
+
+scene.add(plane);
 
 const { array } = plane.geometry.attributes.position
 for (let i=0; i < array.length; i += 3){
@@ -37,7 +42,15 @@ for (let i=0; i < array.length; i += 3){
   array[i + 2] = z + Math.random()
 }
 
-scene.add(plane);
+const colors = [];
+
+for (let i = 0; i < plane.geometry.attributes.position.count; i++){
+  colors.push(1, 0, 0);
+}
+
+plane.geometry.setAttribute('color', new THREE.BufferAttribute(new 
+  Float32Array(colors), 3) 
+  );
 
 // Lightning
 const directionalLight = new THREE.DirectionalLight(0xffffff);
@@ -65,12 +78,32 @@ function moveCamera(){
 
 document.body.onscroll = moveCamera;
 
+// Normalized mouse coordinates
+const mouse = {
+  x: undefined,
+  y: undefined
+}
+
 function animate() {
   requestAnimationFrame( animate );
 
   controls.update();
 
   renderer.render( scene, camera );
+
+  raycaster.setFromCamera(mouse, camera);
+  const intersect = raycaster.intersectObject(plane);
+
+  if (intersect.length > 0){
+    intersect[0].object.geometry.attributes.color.setX[intersect[0].face.a, 0]
+
+    //intersect[0].object.geometry.attributes.color. = true; doesn't work yet
+  }
 }
 
 animate();
+
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX / innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+})
